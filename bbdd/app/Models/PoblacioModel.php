ee3c3edd7cef18f5e150822155b18a7858b9d47e
BlaiBV <120587038+BlaiBV@users.noreply.github.com>
2024-01-09 19:10:12 +0100

    Commit Models Blai

---
+++ bbdd/app/Models/PoblacioModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PoblacioModel extends Model
+{
+    protected $table            = 'poblacio';
+    protected $primaryKey       = 'id_poblacio';
+    protected $useAutoIncrement = false;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['nom_poblacio','id_comarca'];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    public function addCountry($d1,$d2,$d3)
+    {
+        $this->insert([
+            "sortname" => $d1,
+            "name" => $d2,
+            "phonecode" => $d3
+        ]);
+    }
+}
