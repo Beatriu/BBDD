c71f1bd419cd48c6b0f493534702529b870ea75d
Beatriu <BeatriuBadiaSala@gmail.com>
2024-02-16 18:38:15 +0100

    Beatriu: Languages

---
+++ bbdd/app/Language/ca/News.php
@@ -0,0 +1,16 @@
+<?php
+
+// override core en language system validation or define your own en language validation message
+return [
+    'pageCreateTitle'    => 'Pàgina crear noticia',
+    'labelNewsTitle'    => 'Titular noticia',
+    'labelNewsBody'      => 'Contingut noticia',
+    'btnNewsCreate'      => 'Crear noticia',
+    'lnkBackNewsList'      => 'Tornar a noticies',
+    'buttons' => [
+        'create' => 'Crear',
+        'delete' => 'Eliminar',
+        'update' => 'Actualitzar',
+        'cancel' => 'Cancelar',
+    ],
+];
---
+++ bbdd/app/Language/es/crud.php
@@ -0,0 +1,19 @@
+<?php
+
+// override core en language system validation or define your own en language validation message
+return [
+    'pageCreateTitle'    => 'Pàgina crear noticia',
+    'labelNewsTitle'    => 'Titular noticia',
+    'labelNewsBody'      => 'Contingut noticia',
+    'btnNewsCreate'      => 'Crear noticia',
+    'lnkBackNewsList'      => 'Tornar a noticies',
+    'buttons' => [
+        'create' => 'Crear',
+        'delete' => 'Eliminar',
+        'update' => 'Actualitzar',
+        'cancel' => 'Cancelar',
+        'enter' => 'Entrar'
+    ],
+    'user' => 'Usuario',
+    'password' => 'Contraseña'
+];
