b358c1dc5f8018ee983983935f50532cea7dc2dc
Beatriu <BeatriuBadiaSala@gmail.com>
2024-02-19 20:51:24 +0100

    Beatriu:  Nous documents creats per a les noves vistes
    Crear els documents per a les noves vistes i les seves organitzacions i canviar una mica l'html i css de loginSelect

--- bbdd/app/Views/logins/loginSelect.php
+++ bbdd/app/Views/logins/loginSelect.php
@@ -18,21 +18,22 @@
 
 <form class="d-flex align-items-center justify-content-center">
     <div class="w-25 p-3" id="formulari">
-        <div class="form-group d-flex flex-column">
+        <div class="contenidor form-group d-flex flex-column">
             <div class="d-flex justify-content-center">
-            <p class="h6">Institut</p>
+                <p class="titol h6">Institut</p>
             </div>
             <div class="d-flex justify-content-center">
-            <select name="" id="">
-                <option>Caparrella</option>
-                <option>Lladonosa</option>
-            </select>
+                <!-- TODO: Canviar selector per un datalist -->
+                <select class="selector" name="" id="">
+                    <option>Caparrella</option>
+                    <option>Tremp</option>
+                </select>
+            </div>
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-outline-dark"><?= lang('crud.buttons.enter') ?></button>
             </div>
         </div>
         <br />
-        <div class="d-flex justify-content-center">
-        <button class="btn btn-outline-dark"><?= lang('crud.buttons.enter') ?></button>
-        </div>
     </div>
 </form>
 <?= $this->endSection('contingut'); ?>
