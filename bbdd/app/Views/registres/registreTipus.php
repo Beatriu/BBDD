1928876a4f9540ab7db2476d8e4f566cab928cc4
BlaiBV <120587038+BlaiBV@users.noreply.github.com>
2024-06-07 11:51:07 +0200

    Petita modificació Funcionalitat Tipus

--- bbdd/app/Views/registres/registreTipus.php
+++ bbdd/app/Views/registres/registreTipus.php
@@ -99,7 +99,6 @@
             </ul>
         </div>
 
-        <?php if($tipus_pantalla == "tipus_inventari"): ?> 
 
             <div class="col-sm p-0 pb-3 ps-5 pe-5 min-vh-100" id="zona_taula">
                 <div class="row mt-5 justify-content-center">
@@ -135,6 +134,9 @@
                     </div>
 
                 </div>
+
+            <?php if($tipus_pantalla == "tipus_inventari"): ?> 
+
                 <div>
                     <?php if ((session()->get('tipus_inventari_buit')) !== null) : ?>
                         <div class="alert alert-warning alerta_esborrar" role="alert">
@@ -176,99 +178,61 @@
                     </div>
                 </form>
 
-                <div class="row">
-                    <div class="col">
-                        <?= $output ?>
-                    </div>
-                </div>
-            </div>
 
-        <?php elseif($tipus_pantalla == "tipus_intervencio"): ?>
+            <?php elseif($tipus_pantalla == "tipus_intervencio"): ?>
 
-            <div class="col-sm p-0 pb-3 ps-5 pe-5 min-vh-100" id="zona_taula">
-                <div class="row mt-5 justify-content-center">
-                    <div class="col-2 d-flex align-items-center">
-                        <a class="btn btn-dark rounded-pill" href="<?= base_url('/tiquets') ?>">
-                            <i class="fa-solid fa-arrow-left"></i> <?= lang('general_lang.tornar') ?>
-                        </a>
-                    </div>
-
-
-                    <div class="col-10 justify-content-left">
-                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                            <div class="container-fluid">
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="menu_tipus">
-                                        <li class="nav-item">
-                                            <?php if ($tipus_pantalla == "tipus_inventari"): ?>
-                                                <a class="nav-link active actiu" aria-current="page" href="<?= base_url('/tipus/inventari') ?>"><?= lang('tipus.tipus_inventari') ?></a>
-                                            <?php else: ?>
-                                                <a class="nav-link" aria-current="page" href="<?= base_url('/tipus/inventari') ?>"><?= lang('tipus.tipus_inventari') ?></a>
-                                            <?php endif; ?>
-                                        </li>
-                                        <li class="nav-item">
-                                            <?php if ($tipus_pantalla == "tipus_intervencio"): ?>
-                                                <a class="nav-link active actiu" aria-current="page" href="<?= base_url('/tipus/intervencio') ?>"><?= lang('tipus.tipus_intervencio') ?></a>
-                                            <?php else: ?>
-                                                <a class="nav-link" aria-current="page" href="<?= base_url('/tipus/intervencio') ?>"><?= lang('tipus.tipus_intervencio') ?></a>
-                                            <?php endif; ?>
-                                        </li>
-                                    </ul>
-                                </div>
+                    <div>
+                        <?php if ((session()->get('tipus_intervencio_buit')) !== null) : ?>
+                            <div class="alert alert-warning alerta_esborrar" role="alert">
+                                <?= session()->get('tipus_intervencio_buit') ?>
                             </div>
-                        </nav>
+                        <?php endif; ?>
+                        <?php if ((session()->get('tipus_intervencio_existeix')) !== null) : ?>
+                            <div class="alert alert-warning alerta_esborrar" role="alert">
+                                <?= session()->get('tipus_intervencio_existeix') ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ((session()->get('tipus_intervencio_desactivat')) !== null) : ?>
+                            <div class="alert alert-danger alerta_esborrar" role="alert">
+                                <?= session()->get('tipus_intervencio_desactivat') ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ((session()->get('tipus_intervencio_esborrat')) !== null) : ?>
+                            <div class="alert alert-danger alerta_esborrar" role="alert">
+                                <?= session()->get('tipus_intervencio_esborrat') ?>
+                            </div>
+                        <?php endif; ?>
                     </div>
 
-                </div>
-                <div>
-                    <?php if ((session()->get('tipus_intervencio_buit')) !== null) : ?>
-                        <div class="alert alert-warning alerta_esborrar" role="alert">
-                            <?= session()->get('tipus_intervencio_buit') ?>
-                        </div>
-                    <?php endif; ?>
-                    <?php if ((session()->get('tipus_intervencio_existeix')) !== null) : ?>
-                        <div class="alert alert-warning alerta_esborrar" role="alert">
-                            <?= session()->get('tipus_intervencio_existeix') ?>
-                        </div>
-                    <?php endif; ?>
-                    <?php if ((session()->get('tipus_intervencio_desactivat')) !== null) : ?>
-                        <div class="alert alert-danger alerta_esborrar" role="alert">
-                            <?= session()->get('tipus_intervencio_desactivat') ?>
-                        </div>
-                    <?php endif; ?>
-                    <?php if ((session()->get('tipus_intervencio_esborrat')) !== null) : ?>
-                        <div class="alert alert-danger alerta_esborrar" role="alert">
-                            <?= session()->get('tipus_intervencio_esborrat') ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-
-                <div class="row border mt-4 ms-1 me-0 pe-0 ps-0">
-                    <div class="row form_header p-3 ms-0">
-                        
-                    </div>
-                </div>
-
-                <form method="POST" action="/tipus/intervencio/afegir" enctype="multipart/form-data">
-                    <?= csrf_field() ?>
-                    <div class="row">
-                        <div class="col-9 mt-4">
-                            <input type="text" class="form-control" name="tipus_intervencio" id="tipusIntervencio" placeholder="<?= lang('tipus.escriu_tipus_intervencio') ?>" required>
-                        </div>
-                        <div class="col-3 mt-4 d">
-                            <button type="submit" class="btn btn-success rounded-pill"><i class="fa-solid fa-plus"></i> <?= lang('tipus.afegir_tipus_intervencio') ?></button>
+                    <div class="row border mt-4 ms-1 me-0 pe-0 ps-0">
+                        <div class="row form_header p-3 ms-0">
+                            
                         </div>
                     </div>
-                </form>
 
-                <div class="row">
-                    <div class="col">
+                    <form method="POST" action="/tipus/intervencio/afegir" enctype="multipart/form-data">
+                        <?= csrf_field() ?>
+                        <div class="row">
+                            <div class="col-9 mt-4">
+                                <input type="text" class="form-control" name="tipus_intervencio" id="tipusIntervencio" placeholder="<?= lang('tipus.escriu_tipus_intervencio') ?>" required>
+                            </div>
+                            <div class="col-3 mt-4 d">
+                                <button type="submit" class="btn btn-success rounded-pill"><i class="fa-solid fa-plus"></i> <?= lang('tipus.afegir_tipus_intervencio') ?></button>
+                            </div>
+                        </div>
+                    </form>
+
+            <?php endif; ?>
+
+            <div class="row">
+                <div class="col">
                         <?= $output ?>
-                    </div>
                 </div>
             </div>
+            
+        </div>
 
-        <?php endif; ?>
+
 
     </div>
 
